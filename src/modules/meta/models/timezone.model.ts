e0d71fd2eea8264a781562f19b6cabe75b429a2f
Андрей Черников <vtb4058811@region.vtb.ru>
2023-09-02 19:56:28 +0500

    Разработка нового функционала:
    - GH-12, реализован запрос на получение данных по текущей сессии. - GET: "/api/session".
    - Добавлены заглушки на будущие запросы: Смена пароля, сброс пароля, удаление сессии другого устройства, удаление всех сессий, кроме текущего устройства.
    
    + Документация кода.

--- src/modules/meta/models/timezone.model.ts
+++ src/modules/meta/models/timezone.model.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from "@nestjs/swagger";
 import { HydratedDocument, Model, Types } from 'mongoose';
 
 export type TTimezoneDocument = HydratedDocument<Timezone>;
@@ -6,18 +7,36 @@ export type TTimezoneModel = Model<TTimezoneDocument>;
 
 @Schema()
 export class Timezone {
+  @ApiProperty({
+    name: "countryCode",
+    required: true,
+    type: String,
+    description: "Код страны"
+  })
   @Prop({
     type: String,
     required: true,
   })
   countryCode: string;
 
+  @ApiProperty({
+    name: "countryName",
+    required: true,
+    type: String,
+    description: "Полное название страны"
+  })
   @Prop({
     type: String,
     required: true,
   })
   countryName: string;
 
+  @ApiProperty({
+    name: "zoneName",
+    type: String,
+    required: true,
+    description: "Название часового пояса",
+  })
   @Prop({
     type: String,
     required: true,
@@ -25,12 +44,24 @@ export class Timezone {
   })
   zoneName: string;
 
+  @ApiProperty({
+    name: "gmtOffset",
+    type: String,
+    required: true,
+    description: 'Смещение часового пояса от GMT в секундах.'
+  })
   @Prop({
     type: Number,
     required: true,
   })
   gmtOffset: number;
 
+  @ApiProperty({
+    name: "_id",
+    type: String,
+    required: true,
+    description: 'Уникальный идентификатор часового пояса.'
+  })
   _id: Types.ObjectId;
 }
 
--- src/modules/session/session.controller.ts
+++ src/modules/session/session.controller.ts
@@ -1,10 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  Headers,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
   Req,
   Res,
@@ -12,7 +13,15 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiCookieAuth, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCookieAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { ResponseAdapter } from '../../utils/adapters/response.adapter';
 import { EXCEPTION_TYPES } from '../../utils/exception.data';
@@ -26,19 +35,21 @@ import { ApiUserResponseDto } from '../profile/dto/api.user.response-dto';
 import { TProfileExceptionCodes } from '../profile/profile.exception';
 import { Profile, TProfileDocument } from '../profile/profile.model';
 import { WalletsService } from '../wallets/services/wallets.service';
+import { SessionDataAdapterDto, SessionDataResponseDto } from './dto/session.data.adapter.dto';
 import { SessionDto } from './dto/session.dto';
 import { COOKIE_NAMES, SESSION_MESSAGES } from './session.constants';
-import { GetCookies, UserSession, UserInfo } from "./session.decorators";
+import { GetCookies, UserSession } from './session.decorators';
 import { SessionGuard } from './session.guard';
 import { SessionService } from './session.service';
 import { SignInServiceMethodReturned } from './types/session.types';
-import { ISessionData } from "./utils/session.data";
+import { ISessionData } from './utils/session.data';
 
 @ApiTags(SWAGGER_TAGS.SESSION)
 @Controller('session')
 export class SessionController {
-  constructor(private readonly sessionService: SessionService, private readonly walletService: WalletsService) {}
-
+  constructor(private readonly sessionService: SessionService, private readonly walletService: WalletsService) {
+  }
+  
   @UsePipes(new ValidationPipe())
   @Post('signUp')
   @ApiOperation({ summary: 'Регистрация пользователя' })
@@ -51,23 +62,23 @@ export class SessionController {
   @ApiResponse(DEFAULT_SWAGGER_RESPONSE)
   async register(@Body() dto: SessionDto): Promise<ApiUserResponseDto> {
     const result: TProfileDocument | TProfileExceptionCodes = await this.sessionService.registerUser(dto);
-
+    
     if (typeof result === 'string') {
       throw ExceptionFactory.create({ moduleName: 'profile', code: result }, null);
     }
-
+    
     const wallets = await this.walletService.createBaseWallets(result._id);
-
+    
     if (!wallets.length) {
       throw ExceptionFactory.create({ moduleName: 'session', code: 'REG_CANT_CREATE_BASE_WALLETS' }, null);
     }
-
+    
     return new ResponseAdapter(result, {
       type: EXCEPTION_TYPES.SUCCESS,
       message: SESSION_MESSAGES.USER_SUCCESSFULLY_CREATED,
     });
   }
-
+  
   @UsePipes(new ValidationPipe())
   @Post('signIn')
   @ApiOperation({ summary: 'Авторизация пользователя' })
@@ -85,25 +96,25 @@ export class SessionController {
   ): Promise<Response<ApiUserResponseDto>> {
     const data: SignInServiceMethodReturned | AnyModuleExceptionObject | RejectException =
       await this.sessionService.signInUser(dto, req, res);
-
+    
     if ('moduleName' in data && 'code' in data) {
       throw ExceptionFactory.create(data, null);
     }
-
+    
     if (data instanceof RejectException) {
       throw data;
     }
-
+    
     this.sessionService.setSessionTokenToResponse(data.token, res);
-
+    
     const result: ApiUserResponseDto = new ResponseAdapter(data.userInfo, {
       type: EXCEPTION_TYPES.SUCCESS,
       message: SESSION_MESSAGES.USER_SUCCESS_LOGIN,
     });
-
+    
     return res.status(HttpStatus.OK).json(result);
   }
-
+  
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   @ApiCookieAuth(COOKIE_NAMES.ACCESS_TOKEN)
@@ -118,17 +129,30 @@ export class SessionController {
     this.sessionService.removeTokenFromCookie(res);
   }
   
+  @Post('password/change')
+  @HttpCode(HttpStatus.OK)
+  @ApiCookieAuth(COOKIE_NAMES.ACCESS_TOKEN)
+  @ApiOperation({ summary: 'Изменить пароль пользователя' })
+  async changeUserPassword() {
+  }
+  
+  @Post('password/reset')
+  @HttpCode(HttpStatus.OK)
+  @ApiCookieAuth(COOKIE_NAMES.ACCESS_TOKEN)
+  @ApiOperation({ summary: 'Сбросить пароль пользователя и отправить новый, сгенерированный сервером пароль на email' })
+  async resetUserPassword() {
+  }
+  
   @Get()
   @HttpCode(HttpStatus.OK)
-  @ApiOperation({summary: "Проверить текущую сессию и получить данные профиля"})
+  @ApiOperation({ summary: 'Проверить текущую сессию и получить данные профиля' })
   @ApiCookieAuth(COOKIE_NAMES.ACCESS_TOKEN)
+  @ApiOkResponse({ type: SessionDataResponseDto, description: 'Сессия корректна, данные найдены.' })
   @UseGuards(SessionGuard)
-  async checkCurrentSession(
-    @UserInfo() user: Profile
-  ){
-    return new ResponseAdapter(user)
+  async getCurrentSession(@UserSession() sessionData: ISessionData): Promise<SessionDataResponseDto> {
+    return new ResponseAdapter(new SessionDataAdapterDto(sessionData));
   }
-
+  
   @Get('all')
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Получить все активные сессии' })
@@ -138,5 +162,26 @@ export class SessionController {
     const result = await this.sessionService.getActiveSessions(session);
     
     return new ResponseAdapter(result);
+  }
+  
+  @Delete(':sessionUUID')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Закрыть сессию на другом устройстве' })
+  @ApiParam({
+    name: 'sessionUUID',
+    description: "Уникальный идентификатор сессии, получить можно из запроса /api/session/all, ключ 'uuid'",
+    type: String,
+  })
+  @UseGuards(SessionGuard)
+  @ApiCookieAuth(COOKIE_NAMES.ACCESS_TOKEN)
+  async closeSession(@UserSession() session: ISessionData, @Param('sessionUUID') uuid: string) {
+  }
+  
+  @Delete('all')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Завершить все сессии, кроме текущей' })
+  @UseGuards(SessionGuard)
+  @ApiCookieAuth(COOKIE_NAMES.ACCESS_TOKEN)
+  async closeAllSessionsWithoutCurrent() {
   }
 }
